cc975d24eb051183965c6e90b8e648a5af025877
pzsuzsa <prekub.zsuzsanna@gmail.com>
2023-11-08 17:54:48 +0100

    extending resources, adding Feladat15-16_gyakorlas

---
+++ Feladat15-16_gyakorlas/gyakorlas.js
@@ -0,0 +1,7 @@
+function buttonClicked() {
+    alert("Biztos, hogy a kosárba helyezi?")
+}
+
+function otherButtonClicked(zip,city,street,housenr){
+    alert("Köszönjük a vásárlást! Termékeit az alábbi címre postázzuk: "+ zip + ' ' + city + ' ' + street + ' ' + housenr)
+}
